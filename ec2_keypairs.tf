6780c818f078f1773e7eb5f2200bf42a5c640377
Farhan Taib <mohdfarhantaib@outlook.com>
2020-02-14 12:18:37 +0800

    Update ec2_keypairs.tf

--- ec2_keypairs.tf
+++ ec2_keypairs.tf
@@ -1,6 +1,6 @@
 # Cheetah DevOps SSH Key for Root only 
 # Bastion
-resource "aws_key_pair" "devops" {
-  key_name   = "devops"
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
   public_key = "your key pairs here"
 }
